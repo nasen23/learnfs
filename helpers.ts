6251f67fc179ae6fb0b620bdb37bc295badb2944
Sen Na <i@nasen.xyz>
2020-06-02 13:14:02 +0800

    Initialize course list in 'init'

--- helpers.ts
+++ helpers.ts
@@ -4,8 +4,19 @@ export function stat(st) {
     atime: st.atime || new Date(),
     ctime: st.ctime || new Date(),
     size: st.size !== undefined ? st.size : 0,
-    mode: st.mode === 'dir' ? 16877 : (st.mode === 'file' ? 33188 : (st.mode === 'link' ? 41453 : st.mode)),
+    mode:
+      st.mode === 'dir'
+        ? 16877
+        : st.mode === 'file'
+        ? 33188
+        : st.mode === 'link'
+        ? 41453
+        : st.mode,
     uid: st.uid !== undefined ? st.uid : process.getuid(),
-    gid: st.gid !== undefined ? st.gid : process.getgid()
-  }
-};
+    gid: st.gid !== undefined ? st.gid : process.getgid(),
+  };
+}
+
+export function directory(entries: string[]): string[] {
+  return ['.', '..', ...entries];
+}
